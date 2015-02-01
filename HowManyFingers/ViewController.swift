367918fcf8cfd9694d3409e7b5f6bad6e7743ad4
WeiXiang123 <jerry@tootootekiMacBook-Air.local>
2015-02-01 16:25:46 +0800

    first App
    first app for study only

--- HowManyFingers/ViewController.swift
+++ HowManyFingers/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guess: UITextField!
+
+    @IBOutlet weak var resultLable: UILabel!
+
+    @IBAction func guessButton(sender: UIButton) {  
+
+
+        var randomNum:Int = Int(arc4random_uniform(6))
+        var guessNum = guess.text.toInt()
+        if let guessNum = guessNum {
+            if randomNum == guessNum  {
+                resultLable.text = "Correct!"
+            }else {
+                resultLable.text = "Nope! it was \(randomNum)"
+            }
+        }else {
+            resultLable.text = "enter 0~5"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
